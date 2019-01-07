7481386970167e2b33958ce7273b3d7d5aef8862
Vladdd97 <vladbantus@mail.ru>
2019-01-07 20:05:22 +0200

    # Add new Spring-Boot project
    - added new Spring-Boot project with all required dependencies;

---
+++ src/main/java/com/faf/twitterCloneApp/TwitterCloneAppApplication.java
@@ -0,0 +1,12 @@
+package com.faf.twitterCloneApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneAppApplication.class, args);
+	}
+}
